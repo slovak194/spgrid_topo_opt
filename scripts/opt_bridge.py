dfc64dbaed13de1be4e7e235743cb5f59e10b3b9
Oleksandr Slovak <slovak194@gmail.com>
2020-03-20 00:32:12 +0100

    get original bridge back

--- scripts/opt_bridge.py
+++ scripts/opt_bridge.py
@@ -3,24 +3,20 @@ import taichi as tc
 
 version = 8
 wireframe = False
-narrow_band = True
+narrow_band = False
 plane_force = True
 volume_fraction=0.08
 
-use_mirror = False
+use_mirror = True
 
 # Initialize
-n = 1024
+n = 1800
 #tc.core.set_core_trigger_gdb_when_crash(True);
 opt = TopoOpt(res=(n, n, n), version=version, volume_fraction=volume_fraction,
               grid_update_start=5 if narrow_band else 1000000,
               progressive_vol_frac=5, cg_tolerance=1e-3,
               minimum_stiffness=0, minimum_density=1e-2,
-              fix_cells_at_dirichlet=True,
-              fix_cells_near_force=True,
-              connectivity_filtering=True,
-              adaptive_min_fraction=False,
-              verbose_snapshot=False)
+              fix_cells_at_dirichlet=False, fix_cells_near_force=True, connectivity_filtering=True, adaptive_min_fraction=False, verbose_snapshot=False)
 
 x, y, z = 0.1, 0.1, 0.4
 if use_mirror:
@@ -39,7 +35,7 @@ eps = 0.02
 
 if plane_force:
   s = z - eps
-  opt.add_plane_load(force=(0, 1e4, 0), axis=1, extreme=-1, bound1=(-s, -s, -s), bound2=(s, s, s))
+  opt.add_plane_load(force=(0, -1e4, 0), axis=1, extreme=-1, bound1=(-s, -s, -s), bound2=(s, s, s))
 else:
   opt.add_load(center=(0.0, -y + 0.01, 0.0), force=(0, -1e6, 0))
 
